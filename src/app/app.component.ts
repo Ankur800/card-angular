47775fee75d88bb671e949e321210be8ac5e516b
Ankur Rai <ankurrai@Ankurs-MacBook-Pro.local>
2023-10-17 22:33:40 +0530

    completed card app in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+    posts = [
+        {
+            title: 'Neat Tree',
+            imageUrl: 'assets/tree.jpeg',
+            username: 'nature',
+            content: 'Saw this awesome tree during my hike today',
+        },
+        {
+            title: 'Snowy Mountain',
+            imageUrl: 'assets/mountain.jpeg',
+            username: 'mountainlover',
+            content: 'Here is a picture of snowny mountain',
+        },
+        {
+            title: 'Mountain Biking',
+            imageUrl: 'assets/biking.jpeg',
+            username: 'biking122',
+            content: 'I did some biking today',
+        },
+        {
+            title: 'Mountain Biking',
+            imageUrl: 'assets/biking.jpeg',
+            username: 'biking122',
+            content: 'I did some biking today',
+        },
+    ];
 }
